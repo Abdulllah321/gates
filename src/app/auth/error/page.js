3351dba96392f2a17101c8aef194a3d89e5a1142
Abdulllah321 <abdullahsufyan2007@gmail.com>
2024-10-25 02:04:28 +0500

    "Refactor Error page component to use searchParams instead of props, and remove getServerSideProps function."

--- src/app/auth/error/page.js
+++ src/app/auth/error/page.js
@@ -1,16 +1,10 @@
-// pages/auth/error.js
-export default function Error({ error }) {
+export default function ErrorPage({ searchParams }) {
+  const error = searchParams.error || "An unexpected error occurred.";
+
   return (
     <div>
       <h1>Error</h1>
       <p>{error}</p>
     </div>
   );
-}
-
-export async function getServerSideProps(context) {
-  const { error } = context.query;
-  return {
-    props: { error: error || "An unexpected error occurred." },
-  };
 }
