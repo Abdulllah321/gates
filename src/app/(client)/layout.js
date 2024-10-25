daa43e882065ef7d2ce2f0d0c841af4c9ce9b0b0
Abdulllah321 <abdullahsufyan2007@gmail.com>
2024-10-25 18:10:22 +0500

    "Wrap main content with React Suspense component"

--- src/app/(client)/layout.js
+++ src/app/(client)/layout.js
@@ -1,12 +1,15 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import CartElement from "@/components/Cart";
+import { Suspense } from "react";
 
 export default async function RootLayout({ children }) {
   return (
     <>
       <Header />
-      <main className="mt-[62px] min-h-[100vh] relative">{children}</main>
+      <main className="mt-[62px] min-h-[100vh] relative">
+        <Suspense>{children}</Suspense>
+      </main>
       <Footer />
     </>
   );
