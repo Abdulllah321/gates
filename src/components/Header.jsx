e0a98de7923886228455a122355324ed70b72380
Abdulllah321 <135146017+Abdulllah321@users.noreply.github.com>
2024-11-17 08:30:12 +0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,7 +97,7 @@ const Header = () => {
           id="logo"
           aria-label="Website logo"
         >
-          <Image src="/logo.png" width={120} height={100} alt="Logo" />
+          <Image src="/IMG-20241117-WA0059.jpg" width={120} height={100} alt="Logo" />
         </div>
 
         {/* Right Navigation */}
